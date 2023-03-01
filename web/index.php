60507c427f2c85f22dc16b145203a62b27d45ef0
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-03-01 08:36:40 +0200

    init

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<?php
+//TODO main layout, add bootstrap & jquery, form handle in register php (must return html?)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+aaa
+</body>
+</html>
